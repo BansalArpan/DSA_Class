04b74ecc996c8687ed1b24219611423f02a86d62
Arpan <145303752+The-Infern0@users.noreply.github.com>
2026-09-26 13:36:18 -0400

    completed Part1

--- MP2/ECE2230_MP2_Starter_Fall2026/driver.c
+++ MP2/ECE2230_MP2_Starter_Fall2026/driver.c
@@ -11,20 +11,20 @@ easiest algorithm and it depends only on llist_construct.
    testing only, and you will NOT submit it as it is for your personal testing
    during development.
 
-Below are a few simple examples that allocates a few structures and inserts 
+Below are a few simple examples that allocates a few structures and inserts
 them into a list.
 
-With this file, you can compile and run llist.c on its own using 
+With this file, you can compile and run llist.c on its own using
 
    gcc -Wall -g driver.c llist.c ids_support.c -o driver
 
 The compiling command is also in the makefile. Do
-   make driver 
+   make driver
 
-Do this testing before you make ANY changes to ids_support.c.  The only 
+Do this testing before you make ANY changes to ids_support.c.  The only
 reason we need to compile ids_support.c is for the ids_compare function.
 The linker needs to find the compare_fun function and it is found in the template
-   ids_support.c file (with no changes) 
+   ids_support.c file (with no changes)
 */
 
 #include <stdlib.h>
@@ -44,108 +44,193 @@ int main(void)
 
     printf("\nFirst test of unsorted insert.  Should insert 555\n\n");
     // create one item to test llist_insert
-    datap = (data_t *) calloc(1, sizeof(data_t));
+    datap = (data_t *)calloc(1, sizeof(data_t));
     datap->generator_id = 555;
     llist_insert(unsorted_list, datap, LLPOSITION_FRONT);
     datap = NULL;
 
     // test llist_access with one item in list
     datap = llist_access(unsorted_list, LLPOSITION_FRONT);
-    if (datap != NULL) {
+    if (datap != NULL)
+    {
         printf("access looked for first item and found (%d)\n", datap->generator_id);
-    } else {
+    }
+    else
+    {
         printf("ERROR: list access is broken?\n");
     }
     datap = NULL;
 
     // add a second item  to tail of the list
-    datap = (data_t *) calloc(1, sizeof(data_t));
+    datap = (data_t *)calloc(1, sizeof(data_t));
     datap->generator_id = 333;
     llist_insert(unsorted_list, datap, LLPOSITION_BACK);
     datap = NULL;
 
     // add a third item in the middle
-    datap = (data_t *) calloc(1, sizeof(data_t));
+    datap = (data_t *)calloc(1, sizeof(data_t));
     datap->generator_id = 444;
     llist_insert(unsorted_list, datap, 1);
     datap = NULL;
 
     printf("\nSecond test. Inserted 333 and 444.  Use access to find them. List should be {555, 444, 333}\n\n");
-    // find all three and print 
-    datap = llist_access(unsorted_list, 0);   /* same as LLPOSITION_FRONT */
-    if (datap != NULL) {
+    // find all three and print
+    datap = llist_access(unsorted_list, 0); /* same as LLPOSITION_FRONT */
+    if (datap != NULL)
+    {
         printf("Position 0 should find 555 and found (%d)\n", datap->generator_id);
         datap = llist_access(unsorted_list, 1);
         printf("Position 1 should find 444 and found (%d)\n", datap->generator_id);
         datap = llist_access(unsorted_list, LLPOSITION_BACK);
         printf("Position 2 should find 333 and found (%d)\n", datap->generator_id);
-    } else {
+    }
+    else
+    {
         printf("ERROR: list access is broken?\n");
     }
 
-    //Next try to use llist_debug_print 
+    // Next try to use llist_debug_print
     printf("\nRepeat prints to test the list debug print function.  It should print same list.\n\n");
     llist_debug_print(unsorted_list);
 
-    // you should repeat the above tests with differents insertion orders
+    // repeat the above tests with different insertion orders
+    printf("\nRepeat unsorted insert tests with a different insertion order.\n");
+    llist_t *order_test_list = llist_construct(ids_compare_genid);
+
+    datap = (data_t *)calloc(1, sizeof(data_t));
+    datap->generator_id = 100;
+    llist_insert(order_test_list, datap, LLPOSITION_BACK);
+
+    datap = (data_t *)calloc(1, sizeof(data_t));
+    datap->generator_id = 200;
+    llist_insert(order_test_list, datap, LLPOSITION_FRONT);
+
+    datap = (data_t *)calloc(1, sizeof(data_t));
+    datap->generator_id = 990;
+    llist_insert(order_test_list, datap, 1);
+
+    datap = llist_access(order_test_list, 0);
+    if (datap != NULL)
+    {
+        printf("Order test: position 0 should be 200 and is %d\n", datap->generator_id);
+        datap = llist_access(order_test_list, 1);
+        printf("Order test: position 1 should be 999 and is %d\n", datap->generator_id);
+        datap = llist_access(order_test_list, LLPOSITION_BACK);
+        printf("Order test: position 2 should be 100 and is %d\n", datap->generator_id);
+    }
+    else
+    {
+        printf("ERROR: different-order insert test failed\n");
+    }
+
+    llist_destruct(order_test_list);
 
     //*********************************************************************
-    // next you may want to work on llist_insert_sorted, 
- 
+    // next you may want to work on llist_insert_sorted,
+
     printf("\nNext test using new list and sorted inserts.  About to insert 31, 7, 15, and 63.\n\n");
     llist_t *Lsortptr = llist_construct(ids_compare_genid);
 
-    datap = (data_t *) calloc(1,sizeof(data_t));
+    datap = (data_t *)calloc(1, sizeof(data_t));
     datap->generator_id = 31;
     llist_insert_sorted(Lsortptr, datap);
 
     // add a second item in front
-    datap = (data_t *) calloc(1,sizeof(data_t));
+    datap = (data_t *)calloc(1, sizeof(data_t));
     datap->generator_id = 7;
     llist_insert_sorted(Lsortptr, datap);
 
     // add a third item in middle
-    datap = (data_t *) calloc(1,sizeof(data_t));
+    datap = (data_t *)calloc(1, sizeof(data_t));
     datap->generator_id = 15;
     llist_insert_sorted(Lsortptr, datap);
 
     // add a fourth item to end
-    datap = (data_t *) calloc(1,sizeof(data_t));
+    datap = (data_t *)calloc(1, sizeof(data_t));
     datap->generator_id = 63;
     llist_insert_sorted(Lsortptr, datap);
 
     printf("\nHere is the sorted list. The print should show {7, 15, 31, 63}\n\n");
     llist_debug_print(Lsortptr);
- 
+
     // after the insertion functions work, you can try the find command
- 
+
     printf("\nTest of list elem find position on the unsorted list\n");
     int position = -1;
     data_t my_template;
     my_template.generator_id = 444;
     data_t *foundp = llist_elem_find(unsorted_list, &my_template, &position, ids_compare_genid);
-    if (position != -1) {
+    if (position != -1)
+    {
         printf("Alert %d found in position %d\n", my_template.generator_id, position);
         data_t *foundp2 = llist_access(unsorted_list, position);
-        if (foundp != NULL && foundp2 != NULL && foundp == foundp2) {
+        if (foundp != NULL && foundp2 != NULL && foundp == foundp2)
+        {
             printf("looked for %d and found %d in position %d\n",
-                    my_template.generator_id, foundp->generator_id, position);
-            if (my_template.generator_id != foundp->generator_id) {
+                   my_template.generator_id, foundp->generator_id, position);
+            if (my_template.generator_id != foundp->generator_id)
+            {
                 printf("ERROR: returned pointer to wrong record\n");
             }
-        } else {
+        }
+        else
+        {
             printf("ERROR: access did not get position %d\n", position);
         }
-    } else {
+    }
+    else
+    {
         printf("ERROR: looked for %d and did not find it!\n", my_template.generator_id);
     }
 
     // *************************************************************/
-    printf("\n\nHow are you going to test the remove function?\n");
-    //  you should add tests here
+    printf("\n\nTest the remove function on a small unsorted list\n");
+    llist_t *remove_list = llist_construct(ids_compare_genid);
+    for (int id = 10; id <= 40; id += 10)
+    {
+        datap = (data_t *)calloc(1, sizeof(data_t));
+        datap->generator_id = id;
+        llist_insert(remove_list, datap, LLPOSITION_BACK);
+    }
+    llist_debug_print(remove_list);
 
- 
+    data_t *removed = llist_remove(remove_list, LLPOSITION_FRONT);
+    if (removed != NULL)
+    {
+        printf("Removed front item with generator ID %d\n", removed->generator_id);
+        free(removed);
+    }
+    llist_debug_print(remove_list);
+
+    removed = llist_remove(remove_list, 1);
+    if (removed != NULL)
+    {
+        printf("Removed middle item with generator ID %d\n", removed->generator_id);
+        free(removed);
+    }
+    llist_debug_print(remove_list);
+
+    removed = llist_remove(remove_list, LLPOSITION_BACK);
+    if (removed != NULL)
+    {
+        printf("Removed tail item with generator ID %d\n", removed->generator_id);
+        free(removed);
+    }
+    llist_debug_print(remove_list);
+
+    removed = llist_remove(remove_list, 99);
+    if (removed == NULL)
+    {
+        printf("Invalid remove index correctly returned NULL\n");
+    }
+    else
+    {
+        printf("ERROR: invalid remove index did not return NULL\n");
+        free(removed);
+    }
+
     // when done with testing clean up
+    llist_destruct(remove_list);
     llist_destruct(unsorted_list);
     llist_destruct(Lsortptr);
     return 0;
@@ -162,14 +247,16 @@ int main(void)
    found in ex3.c (which we studied in class).
 */
 
-void llist_debug_print(llist_t *list_ptr) {
+void llist_debug_print(llist_t *list_ptr)
+{
     llist_elem_t *rover;
     int count;
 
     printf("list size: %d\n", list_ptr->ll_entry_count);
     rover = list_ptr->ll_front;
     count = 0;
-    while(rover != NULL) {
+    while (rover != NULL)
+    {
         printf("Pos: %d contains ID: %d\n", count, rover->data_ptr->generator_id);
         rover = rover->ll_next;
         count++;
@@ -177,4 +264,3 @@ void llist_debug_print(llist_t *list_ptr) {
     if (count != list_ptr->ll_entry_count)
         printf("\nERROR: the list size is wrong! count is %d\n", count);
 }
-
